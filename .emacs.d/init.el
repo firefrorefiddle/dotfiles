7a704fdde24e0fd22227f4b3c911ff56a9f363c5
Michael Hartl <mikehartl17@gmail.com>
2015-02-26 15:24:58 +0100

    set EDITOR to new alias ec=emacsclient

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,12 @@
 
+;; don't ask confirmation when killing client buffers
+(add-hook 'server-switch-hook
+      (lambda ()
+        (when (current-local-map)
+          (use-local-map (copy-keymap (current-local-map))))
+        (when server-buffer-clients
+          (local-set-key (kbd "C-x k") 'server-edit))))
+
+;; Haskell Mode configuration (haskell-mode must be
+;; installed through the package manager though
 (load-file "haskell-mode.el")
