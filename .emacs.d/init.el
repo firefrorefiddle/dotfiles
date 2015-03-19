4cdb2bf99afce44783e8632863d2fd5666e08f53
Michael Hartl <mikehartl17@gmail.com>
2015-03-19 14:38:28 +0100

    add ELPA packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,6 +7,15 @@
         (when server-buffer-clients
           (local-set-key (kbd "C-x k") 'server-edit))))
 
+;; MELPA repository packages
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
 ;; Haskell Mode configuration (haskell-mode must be
 ;; installed through the package manager though
 (load-file "~/.emacs.d/haskell-mode.el")
