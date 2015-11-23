af1e0a05ccebed560d9a17724d19c96f4908fe76
Michael Hartl <mikehartl17@gmail.com>
2015-11-23 17:00:59 +0100

    some updates to dotfiles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -34,10 +34,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(safe-local-variable-values (quote ((haskell-process-use-ghci . t) (haskell-indent-spaces . 4)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(add-to-list 'auto-mode-alist '("\\.pl\\'" . prolog-mode))
+(add-to-list 'auto-mode-alist '("\\.m\\'" . mercury-mode))
+
+(eval-after-load 'flycheck
+  '(require 'flycheck-mercury))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
