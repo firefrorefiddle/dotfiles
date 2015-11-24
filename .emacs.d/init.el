693164faaa0a7fb8c535def2a3a6a9b9acd37741
Michael Hartl <mikehartl17@gmail.com>
2015-11-24 13:50:17 +0100

    repair init.el after recent merge

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,7 +38,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
  '(inhibit-startup-screen t)
  '(safe-local-variable-values (quote ((haskell-process-use-ghci . t) (haskell-indent-spaces . 4)))))
 (custom-set-faces
@@ -46,7 +45,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
 
 (add-to-list 'auto-mode-alist '("\\.pl\\'" . prolog-mode))
 (add-to-list 'auto-mode-alist '("\\.m\\'" . mercury-mode))
